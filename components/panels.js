ef8671e079ee0d77a8d4daed4671c9adb59adccf
ruto-txt <48617610+ruto-txt@users.noreply.github.com>
2020-07-02 00:06:25 +0900

    9panelsのクリック色変化アクションがほぼ完成され申した
    要素名を収めたオブジェクトをどのように導引するか考え中。初期値はベタ張りでもいいかも。
    レイアウトがゴミカスなので後でそこだけ直す予定。レスポンシブ対応するとlayoutがめっちゃ崩れる。長屋のほうがいいのかも？

--- components/panels.js
+++ components/panels.js
@@ -1,48 +1,136 @@
-import React,{useState} from 'react';
+import React,{useState,useMemo,useLayoutEffect} from 'react';
 
 /*
 *　9ボックスパネル
     DBから内容を入れて項目に利用するかもしれないので、そのような設計を心がける
     状態によって表示を変える
-*/
+    */
 export function Panels(props){
-    // ステートの定義：今の状態
     const [selected,setSelect]=useState([null,null]);
-    //記名ルールで配列にしてもまったく困らないが、どうしようかな
+    const objects={
+        1:{'id':1,'label':"hako1"},
+        2:{'id':2,'label':"hako2"},
+        3:{'id':3,'label':"hako3"},
+        4:{'id':4,'label':"hako4"},
+        5:{'id':5,'label':"hako5"},
+        6:{'id':6,'label':"hako6"},
+        7:{'id':7,'label':"hako7"},
+        8:{'id':8,'label':"hako8"},
+        9:{'id':9,'label':"hako9"},
+    }
+    
+    const identify=useMemo(()=>{
+        const category = selected[0]
+        const item = selected[1]
+        if(!category&&!item){
+            return "reception"
+        }else if(category&&!item){
+            return "category"
+        }else if(category&&item){
+            return "current"
+        }else{return "reception"}
+    },[selected])
 
     // 1：状態を管理する
     // どんな状態が必要？　今の選択状態（階層の深さ？　選択中の番号？
     // 2：現状の状態によって表示を変える
     // 3：状態を変更する
     // 4：リターンする（できなければsetContentかpropsかな？
-    const SelectState = (props)=>{
-        const a = "選択状態：categoryを選んでくださいｗｗ"
-        return <div>{a}</div>
+
+    function handleSetSelectState(input,state){
+        const category = state[0]
+        const item = state[1]
+        let update =[null,null]
+        //現在の状態を確認する
+        //無選択状態なら、カテゴリに代入する
+        if(!category&&!item){
+            update=[input,null]
+        }else if(category&&!item){
+            //カテゴリ状態なら、一致していれば未選択に戻す。していなければ細目に代入する
+            if(category==input){
+                update=[null,null]
+            }else{
+                update=[category,input]
+            }
+        }else if(category&&item){
+            //細目状態なら、一致していれば確定する。していなければ細目を上書きする。戻るなら戻る
+            if(category==input){
+                update=[null,null]
+            }else if(item==input){
+                alert("うんこが"+[category,item]+"kg出ましたｗｗｗｗ")
+                update=[null,null]
+            }else{
+                update=[category,input]
+            }
+        }else{update=[null,null]}
+
+        setSelect(update)
+        //細目状態なら、一致していれば確定をスロー。
+        //していなければ新しい細目を代入する
+
+
     }
-    // オブジェクトで要素名を準備する……かな？
-    const objects=[
-        {'id':1,'label':"hako1"},
-        {'id':2,'label':"hako2"},
-        {'id':3,'label':"hako3"},
-        {'id':4,'label':"hako4"},
-        {'id':5,'label':"hako5"},
-        {'id':6,'label':"hako6"},
-        {'id':7,'label':"hako7"},
-        {'id':8,'label':"hako8"},
-        {'id':9,'label':"hako9"},
-    ]
+
+    const SelectState = ()=>{
+        const category = selected[0]
+        const item = selected[1]
+        if(identify=="current"){
+            return <div>{item +"の"+ objects[category].label + "が選択されています"}</div>
+        }else if(identify=="category"){
+            return <div>{objects[category].label + "からふさわしいものを選んでください"}</div>
+        }else{
+            return <div>{"カテゴリを選んでください"}</div>
+        }
+    }
     
+    function disprayLabel(key){
+        const category = selected[0]
+        const cateLabel =objects[key].label
+        
+        if(identify=="reception"){//未選択の時
+            return <span>{cateLabel}</span>
+        }else{
+            // const itemLabel =objects[category][key].label
+            const itemLabel =objects[category].label + "の" + key
+
+            //カテゴリ選択のとき
+            if(identify=="category"){
+                return key==category?<span>{cateLabel} <br/>＜戻る</span>:<span>{itemLabel}</span>
+            }
+            //細目選択中のとき
+            if(key==selected[1]){
+                    return <span>{itemLabel} <br/>を選択</span>
+            }else if(key==selected[0]){
+                return <span>{cateLabel} <br/>＜戻る</span>
+            }
+            return <span>{itemLabel}</span>
+        }
+    
+    }
+
+    function calcClassName(key){
+        if(identify=="reception"){
+            return "reception"
+        }else if(identify=="current"){
+            if(key==selected[1]){
+                return "current"
+            }
+        }
+        return key==selected[0]?"categoryBack":"category"
+    }
+
     return (
         <>
         <SelectState/>
         <div className="grid-container">
-            {objects.map((obj,index)=>
-                <div id={obj.id} onClick={()=>setSelect(index+1)}>
-                    <span>{obj.label}</span>
+            {Object.keys(objects).map(key=>
+                <div id={objects[key].id} className={calcClassName(key)}
+                onClick={()=>handleSetSelectState(objects[key].id,selected)}>
+                {disprayLabel(key)}
                 </div>
             )}
         </div>
-            <div><p id="description">{props.children}、ステートテスト：{selected}</p></div>
+        <div><p id="description">{props.children}、ステートテスト：{selected}</p></div>
         <style jsx>{`
             .grid-container{
                 display:grid;
@@ -53,10 +141,25 @@ export function Panels(props){
                 align-items:center;
             }
             .grid-container>div{
-                background:#fca;
                 text-align:center;
                 padding:25% 25%;
                 border-radius: 10px;
+            }
+            .reception{
+                background:skyblue;
+            }
+            .category{
+                transition:0.4s;
+                background:pink;
+            }
+            .categoryBack{
+                transition:0.4s;
+                background:#444;
+                color:whitesmoke;
+            }
+            .current{
+                transition:0.2s;
+                background:salmon;
             }
         `}
         </style>
